b58278dd8fde4c82bf7d11fa320351e23f8ec587
Polina <106979919+kollarbone@users.noreply.github.com>
2023-03-14 15:05:08 +0300

    done

--- src/data/Words.js
+++ src/data/Words.js
@@ -1,30 +1,34 @@
-export const Words = [
-  {
-    word: "사과",
-    trans: "Apple",
-    category: "fruits",
-    image:
-      "https://panagroup-bg.com/sites/default/files/aromat_zelena_jabalka.jpg",
-  },
-  {
-    word: "바나나",
-    trans: "Banana",
-    category: "fruits",
-    image:
-      "https://avatars.mds.yandex.net/i?id=a792d6e457ba095ac95efe65588175668097365b-8194143-images-thumbs&n=13",
-  },
-  {
-    word: "커피",
-    trans: "Coffee",
-    category: "drinks",
-    image:
-      "https://avatars.mds.yandex.net/i?id=2729cee8fccbae810e9397d21abd62d27d9890ae-8338814-images-thumbs&n=13",
-  },
-  {
-    word: "물",
-    trans: "Water",
-    category: "drinks",
-    image:
-      "https://avatars.mds.yandex.net/i?id=80938b222c6a1eb3ac7795120d28313ce4de4d4b-8287363-images-thumbs&n=13",
-  },
-];
+export const Words = {
+  fruits: [
+    {
+      word: "사과",
+      trans: "Apple",
+      category: "fruits",
+      image:
+        "https://panagroup-bg.com/sites/default/files/aromat_zelena_jabalka.jpg",
+    },
+    {
+      word: "바나나",
+      trans: "Banana",
+      category: "fruits",
+      image:
+        "https://avatars.mds.yandex.net/i?id=a792d6e457ba095ac95efe65588175668097365b-8194143-images-thumbs&n=13",
+    },
+  ],
+  drinks: [
+    {
+      word: "커피",
+      trans: "Coffee",
+      category: "drinks",
+      image:
+        "https://avatars.mds.yandex.net/i?id=2729cee8fccbae810e9397d21abd62d27d9890ae-8338814-images-thumbs&n=13",
+    },
+    {
+      word: "물",
+      trans: "Water",
+      category: "drinks",
+      image:
+        "https://avatars.mds.yandex.net/i?id=80938b222c6a1eb3ac7795120d28313ce4de4d4b-8287363-images-thumbs&n=13",
+    },
+  ],
+};
--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -6,17 +6,16 @@ export function Cards() {
   const [flip, setFlip] = useState(false)
   const pathname = window.location.pathname
   const path = pathname.replace(/\\|\//g, '')
+  const temp = Words[path as keyof typeof Words]
     return (
       <div className="mx-auto mt-10">
-        {path === Words[activeCard].category && <>
         <div onClick={()=> setFlip(!flip)} className="bg-pink-200 px-4 py-2 rounded-md w-80 h-40 mx-auto mt-10 flex justify-center items-center hover:scale-105 cursor-pointer" >
-          {flip?<span className="text-2xl">{Words[activeCard].word}</span>:<img className="w-10/12 px-4 py-2 rounded-md" src={Words[activeCard].image} alt={Words[activeCard].word}/>}
+          {flip?<span className="text-2xl">{temp[activeCard].word}</span>:<img className="w-10/12 px-4 py-2 rounded-md" src={temp[activeCard].image} alt={temp[activeCard].word}/>}
         </div>
         <div className="flex justify-center items-center">
           {activeCard !== 0 &&<button onClick={()=> setActiveCard(activeCard-1)} className="bg-yellow-200 px-4 py-2 rounded-md w-15 h-10 mx-auto  mt-10 hover:scale-105 cursor-pointer">prev</button>}
-          {Words.length - 1 !== (activeCard ) && <button onClick={()=> setActiveCard(activeCard+1)} className="bg-yellow-200 px-4 py-2 rounded-md w-15 h-10 mx-auto  mt-10 hover:scale-105 cursor-pointer">next</button>}
+          {temp.length - 1 !== (activeCard ) && <button onClick={()=> setActiveCard(activeCard+1)} className="bg-yellow-200 px-4 py-2 rounded-md w-15 h-10 mx-auto  mt-10 hover:scale-105 cursor-pointer">next</button>}
         </div>
-        </>}
       </div>
     );
   }
